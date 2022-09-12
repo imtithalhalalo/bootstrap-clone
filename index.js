205ab0e8c102ccd04be4c004c0cf73e17f5d2a08
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-12 16:24:11 +0300

    start js validation

---
+++ index.js
@@ -0,0 +1,4 @@
+let fullname = document.querySelector('#name')
+let email = document.querySelector('#email')
+let password = document.querySelector('#pwassword')
+let message = document.querySelector('#message')
